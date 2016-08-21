5d0173ed2e1b350aee34d48a4b0ab88f99d1a23b
Anton Mozgovyi <anton.mozgovyi@gmail.com>
2016-08-21 22:36:00 +0300

    added graph loading

--- util.js
+++ util.js
@@ -6,6 +6,16 @@ function readIntegerFile(name) {
   return data.split('\n').filter(item => item).map((item) => (+item))
 }
 
+function readGraphFromFile(name) {
+  return (
+    fs.readFileSync(name, { encoding: 'utf-8'})
+      .split('\n')
+      .filter(item => item)
+      .map(item => item.split('\t').filter(i => i).map(edge => (+edge)).slice(1))
+  )
+}
+
 module.exports = {
   readIntegerFile
+, readGraphFromFile
 }
